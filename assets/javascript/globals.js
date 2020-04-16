59b8d847bb54fbdb8f217790ffd88e32273a55fc
Chikeobi <chikeobi.njaka@gmail.com>
2020-04-16 11:02:41 -0700

    Completed all functions of the main application. Added more images and tightened the page display.

--- assets/javascript/globals.js
+++ assets/javascript/globals.js
@@ -7,11 +7,13 @@ const COUNTRIES_WITH_PROVINCE_DATA = {
   Canada: "Canada",
   UK: "United Kingdom",
   France: "France",
+  Australia: "Australia",
 };
 const COUNTRIES_WITH_COUNTY_DATA = { USA: "US" };
-
+// Regions that will not be in the country dropdown. These areas are in the COVID-193 data
+const SKIP_REGIONS = ["All", "Africa", "Europe", "Asia", "Oceania", "North America", "South America"];
 const RANDOM_IMAGE_PARTIAL_PATH = "./assets/images/random/random-";
-const RANDOM_IMAGE_COUNT = 7;
+const RANDOM_IMAGE_COUNT = 24;
 const RANDOM_IMAGE_INTERVAL_MILLI = 5000;
 const logIt = false;
 var CONTINENT_REGIONS = { Africa: {}, "North-America": {}, "South-America": {}, Europe: {}, Asia: {}, Oceania: {} };
@@ -20,7 +22,29 @@ var randomImageIntervalTimer;
 // Contains response from Covid-193 API. Almost same as covid193CountryData
 var covid_193_statistics_response;
 //
-var covid_19_coronovirus_statistics_response;
+// contains covid19-statistics information like this:
+//
+// {
+//   "city": "Abbeville",
+//   "province": "South Carolina",
+//   "country": "US",
+//   "lastUpdate": "2020-04-12 23:18:00",
+//   "keyId": "Abbeville, South Carolina, US",
+//   "confirmed": 9,
+//   "deaths": 0,
+//   "recovered": 0
+// },
+// {
+//   "city": "Acadia",
+//   "province": "Louisiana",
+//   "country": "US",
+//   "lastUpdate": "2020-04-12 23:18:00",
+//   "keyId": "Acadia, Louisiana, US",
+//   "confirmed": 99,
+//   "deaths": 5,
+//   "recovered": 0
+// }
+var covid19Stats;
 //
 // contains map of country name vs covid-19 data. Example:
 // "China:{
@@ -40,7 +64,31 @@ var covid_19_coronovirus_statistics_response;
 //     "time": "2020-04-13T17:45:05+00:00"
 //   }
 var covid193CountryData;
-// Array of Covid-193 countries 
+// Array of Covid-193 countries
 var covid193Countries;
 
+var selectedCountry = null;
+var selectedProvince = null;
+var selectedCounty = null;
+// Format of this object is as follows
+// {"USA":{ "Alabama":{confirmed:0,
+//                  deaths:0,
+//                  recovered:0},
+//        "Alaska":{confirmed:0,
+//                  deaths:0,
+//                  recovered:0}
+//        },
+// "Canada":{"British Columbia":{confirmed:0,
+//                  deaths:0,
+//                  recovered:0},
+//           },
+// }
+var provinceStats = {};
 
+/**
+ * Formats numbers by adding commas where needed
+ * @param {*} num
+ */
+function formatNumber(num) {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+}
